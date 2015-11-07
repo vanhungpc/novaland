a5f00702dfaa63e03ccfc7d828ba2e4495daae69
cuonghero <cuonghero@ubuntu>
2015-11-06 20:44:11 -0800

    cuonghero

--- application/views/main/footer.php
+++ application/views/main/footer.php
@@ -1,6 +1,26 @@
-<div class="row">
-	<div class="container">
-
-	</div>
-
+<div class="footer">
+    <div class="row">
+        <div class="col-md-4" style="border-right: 1px solid #fff">
+            <p style="color: red;font-size: 15px;font-weight: bolder; margin-bottom: 5px;padding-left: 10px;">NOVALAND</p>
+            <p style="color: #fff;font-size: 14px;margin-bottom: 5px;padding-left: 10px;">
+                Address: The 16th floor - Saigon Tower -
+                29 Le Duan Boulevard - District 1 - HCM City - Vietnam.<br>
+                Email: <a target="_blank" href="mailto:thienttt.finance@gmail.com" style="color: #fff;">thienttt.finance@gmail.com</a><br>
+                Hotline: (84) 909 288 281
+            </p>
+        </div>
+        <div class="col-md-8">
+            <ul class="bottomnav">
+                <li class="first"><a title="Apartment for rent" href="http://www.apartmentforrent.com.vn/">APARTMENT FOR RENT</a></li>          
+                						<li><a title="Apartment" href="http://www.apartmentforrent.com.vn/apartment/">Apartment</a></li>
+												<li><a title="House" href="http://www.apartmentforrent.com.vn/house/">House</a></li>
+												<li><a title="Villas" href="http://www.apartmentforrent.com.vn/villas/">Villas</a></li>
+												<li><a title="Office" href="http://www.apartmentforrent.com.vn/office/">Office</a></li>
+												<li><a title="Serviced apartment" href="http://www.apartmentforrent.com.vn/serviced-apartment/">Serviced apartment</a></li>
+						                <li class="last"><a title="Contact" href="http://www.apartmentforrent.com.vn/contact.html">CONTACT</a></li>
+            </ul>
+            
+            <p style="color: #fff; font-size: 14px; float: right;font-style: italic "> © 2015  NOVALAND. A company of THANH DO INTL.</p>
+        </div>
+    </div>
 </div>
