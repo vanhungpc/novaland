08e245431049d5ca52181dafcad4f67ad5032229
cuonghero <cuonghero@ubuntu>
2015-11-09 05:43:40 -0800

    cuonghero

--- application/views/main/header.php
+++ application/views/main/header.php
@@ -22,7 +22,7 @@
 							<label class="hotlines margin-right-15">
                                                             Email: <a href="mailto:thienttt.finance@gmail.com"><span>thienttt.finance@gmail.com</span></a><br>
                                                             <div class="enable_call">  Hotline:<a href="tel:+840909288281"> <b> (84) 909 288 281</b></a> </div>
-                                                            <div class="unable_call" style="display:none">  Hotline:<b> (84) 909 288 281</b> </div>
+                                                            <div class="unable_call">  Hotline:<b> (84) 909 288 281</b> </div>
 							</label>
 						</div>
 					</div>
